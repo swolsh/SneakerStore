d20cbe02567b6e4ddafa5f9f41c099e11ad19343
swolsh <supsanik@icloud.com>
2023-06-03 17:47:19 +0600

    Initial Commit

---
+++ SneakerStore/SneakerStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakerStoreApp.swift
+//  SneakerStore
+//
+//  Created by A M on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SneakerStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
